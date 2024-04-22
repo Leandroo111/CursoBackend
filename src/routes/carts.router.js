d309cacf746715f3532c202d0d5ef66fb327621f
Leandro111 <leo.j.guerrero@gmail.com>
2024-04-22 15:45:41 -0300

    Correccion de la 1era pre-entrega

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import CartsManager from '../managers/cartManager.js'
-const path = './src/file/carts.json'
+import ProductsManager from '../managers/productManager.js'
+const cartsPath = './src/file/carts.json'
+const productsPath ='./src/file/products.json'
 
 const router = Router()
-const carts = new CartsManager (path)
+const carts = new CartsManager (cartsPath)
+const products = new ProductsManager(productsPath)
 
 router.post('/', async (req, res,) => {
     const response = await carts.createCarts()
@@ -22,9 +25,16 @@ router.post('/:cid/products/:pid', async (req, res,) => {
     const { cid, pid } = req.params
 
     if(isNaN(pid)) return res.send('El parametro pid debe ser un numero')
+
+    const productFound = await products.getProductById(parseInt(pid))
+
+    if(productFound === 'No se encontro un producto con ese ID' || productFound === 'Ingrese un Id' ) res.send('No se encontro un producto con ese ID')
+        
+    else {
+        const response = await carts.addProductsToCart(parseInt(cid), {product: parseInt(pid), quantity: 1})
+        res.send(response)
+    }
     
-    const response = await carts.addProductsToCart(parseInt(cid), {product: parseInt(pid), quantity: 1})
-    res.send(response)
 })
 
 export default router
